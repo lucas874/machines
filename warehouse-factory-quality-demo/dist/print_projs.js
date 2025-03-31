eead1b8a17cfa335d3588543be889ad855e43d43
lucas874 <lsc500@outlook.dk>
2025-03-31 17:37:11 +0200

    Remove dist folders

--- warehouse-factory-quality-demo/dist/print_projs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const protocol_1 = require("./protocol");
-for (var p of protocol_1.all_projections) {
-    console.log(JSON.stringify(p));
-    console.log();
-    console.log("$$$$");
-    console.log();
-}
