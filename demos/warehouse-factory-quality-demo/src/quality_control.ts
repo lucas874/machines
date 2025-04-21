16c3329d92b1763d19b1c293d2fc9b31de520a1b
lucas874 <lsc500@outlook.dk>
2025-04-21 17:09:45 +0200

    Work on aligning demos

--- demos/warehouse-factory-quality-demo/src/quality_control.ts
+++ demos/warehouse-factory-quality-demo/src/quality_control.ts
@@ -46,9 +46,9 @@ async function main() {
     const machine = createMachineRunnerBT(app, tags, s0_, undefined, projectionInfo.branches, projectionInfo.specialEventTypes)
 
     for await (const state of machine) {
-      console.log("quality control robot. state is:", state.type)
+      console.log("Quality control robot. State is:", state.type)
       if (state.payload !== undefined) {
-        console.log("state payload is:", state.payload)
+        console.log("State payload is:", state.payload)
       }
       console.log()
       const s = state.cast()
