6e0a86501be560238b310c608a2cfad2aaa1d1bc
lucas874 <lsc500@outlook.dk>
2026-01-20 14:45:32 +0100

    Working on rust version of running demos

--- demos/start-demo-rust-test/src/main.rs
+++ demos/start-demo-rust-test/src/main.rs
@@ -22,59 +22,9 @@ pub struct Cli {
     #[arg(short, long)]
     pub log_path: PathBuf,
 }
-/*
-#[derive(Debug, Default)]
-pub struct CommandSpec {
-    program: OsString,
-    args: Vec<OsString>,
-    dir: Option<PathBuf>,
-    stdin: Option<Stdio>,
-    stdout: Option<Stdio>,
-    stderr: Option<Stdio>,
-}
-
-impl CommandSpec {
-    fn new(program: impl Into<OsString>) -> Self {
-        Self {
-            program: program.into(),
-            ..Default::default()
-        }
-    }
-
-    fn arg(mut self, arg: impl Into<OsString>) -> Self {
-        self.args.push(arg.into());
-        self
-    }
-
-    fn args<I, S>(mut self, args: I) -> Self
-    where
-        I: IntoIterator<Item = S>,
-        S: Into<OsString>,
-    {
-        self.args.extend(args.into_iter().map(Into::into));
-        self
-    }
-
-    fn dir(mut self, dir: impl Into<PathBuf>) -> Self {
-        self.dir = Some(dir.into());
-        self
-    }
-} */
 
 const HORIZONTAL: &str = "-h";
 const VERTICAL: &str = "-v";
-
-macro_rules! select_pane_args {
-    ($session:expr, $pane_number:expr) => {
-        &[OsStr::new("-L"), $session, "select-pane", "-t", $pane_number]
-    };
-}
-
-macro_rules! split_with_command {
-    ($session:expr, $direction:expr, $command:expr) => {
-        &[OsStr::new("-L"), $session, "split-window", $direction, $command]
-    };
-}
 
 // set up tracing
 fn init_tracing(path: &Path) {
@@ -114,12 +64,6 @@ fn spawn(command: &mut Command) -> Result<Child> {
     command
         .spawn()
         .with_context(|| format!("failed to spawn {:?} from dir={:?}", command, command.get_current_dir()))
-/* let ax_handle = Command::new("ax")
-        .current_dir(dir)
-        .arg("run")
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn(); */
 }
 
 fn spawn_logged(command: &mut Command) -> Result<Child> {
@@ -131,23 +75,6 @@ fn tmux(args: &[&OsStr], dir: Option<&OsStr>) -> Result<()> {
     run_status(Command::new("tmux")
         .current_dir(dir.unwrap_or_else(|| Path::new(".").as_os_str()))
         .args(args))
-}
-
-//fn tmux_new_detached_session
-/*
-macro_rules! select_pane_args {
-    ($session:expr, $pane_number:expr) => {
-        &[OsStr::new("-L"), $session, "select-pane", "-t", $pane_number]
-    };
-}
-
-*/
-
-fn tmux1(args: &[&str], dir: Option<&OsStr>) -> Result<ExitStatus, Error> {
-    Command::new("tmux")
-        .current_dir(dir.unwrap_or_else(|| Path::new(".").as_os_str()))
-        .args(args)
-        .status()
 }
 
 fn rm(args: &[&OsStr], dir: Option<&OsStr>) -> Result<()> {
@@ -214,8 +141,6 @@ fn split_and_run(session: &OsStr, commands: &[&OsStr], dir: Option<&OsStr>) -> R
             }
             // tmux -L "$session" select-pane -t "%$i"
             // tmux -L "$session" split-window "$split" "${cmds[n]}"
-            //tmux(select_pane_args!(session, &i.to_string()), dir)?;
-            //tmux(split_with_command!(session, direction, commands[n]), dir)?;
             select_pane(&OsString::from(i.to_string()))?;
             split_window_with_command(direction, commands[n])?;
 
@@ -226,121 +151,10 @@ fn split_and_run(session: &OsStr, commands: &[&OsStr], dir: Option<&OsStr>) -> R
     }
     //tmux -L "$session" select-layout -t "$session" tiled
     //tmux -L "$session" attach-session -t $session
-    //tmux(&["-L", session, "select-layout", "-t", session, "tiled"], dir)?;
-    //tmux(&["-L", session, "attach-session", "-t", session], dir)?;
     select_layout()?;
     attach_session()?;
     Ok(())
 }
-
-/* fn split_and_run1(session: &str, commands: Vec<&str>, dir: Option<&OsStr>) -> Result<(), Error> {
-    let mut n = 0;
-    let mut split_round = 0;
-    let base: i32 = 2;
-    while n < commands.len() {
-        if n == 0 {
-            let args = ["-L", session, "new-session", "-d", "-s", session, commands[n]];
-            let result_new_session = tmux1(&args, dir);
-            if result_new_session.is_err() {
-                return Err(result_new_session.unwrap_err())
-            }
-            n += 1;
-        }
-
-        let direction = if split_round % 2 == 0 { HORIZONTAL } else { VERTICAL };
-
-        for i in 0..base.pow(split_round) {
-            if n >= commands.len() {
-                break
-            }
-            // tmux -L "$session" select-pane -t "%$i"
-            // tmux -L "$session" split-window "$split" "${cmds[n]}"
-            let select_pane_result = tmux1(select_pane_args!(session, &i.to_string()), dir);
-            if select_pane_result.is_err() {
-                return Err(select_pane_result.unwrap_err())
-            }
-            let split_result = tmux1(split_with_command!(session, direction, commands[n]), dir);
-            if split_result.is_err() {
-                return Err(split_result.unwrap_err());
-            }
-            n += 1;
-        }
-        split_round += 1;
-
-    }
-    //tmux -L "$session" select-layout -t "$session" tiled
-    //tmux -L "$session" attach-session -t $session
-    let layout_result = tmux1(&["-L", session, "select-layout", "-t", session, "tiled"], dir);
-    if layout_result.is_err() {
-        return Err(layout_result.unwrap_err())
-    }
-    let attach_result = tmux1(&["-L", session, "attach-session", "-t", session], dir);
-    if attach_result.is_err() {
-        return Err(attach_result.unwrap_err())
-    }
-
-    Ok(())
-}
- */
-// delete a file or directory.
-/* fn rm(args: &[&OsStr], current_dir: &OsStr) -> Result<ExitStatus, Error> {
-    let _span = tracing::info_span!("rm").entered();
-    tracing::info!("rm {:?} from {}", args.iter().map(|arg| arg.display()).collect::<Vec<_>>(), current_dir.display());
-    let exit_status = Command::new("rm")
-        .current_dir(current_dir)
-        .args(args)
-        .status();
-    tracing::info!("rm {:?} from {}. exit status: {:?}", args.iter().map(|arg| arg.display()).collect::<Vec<_>>(), current_dir.display(), exit_status);
-    exit_status
-} */
-
-
-
-/* fn run_status(program: &OsStr, args: &[&OsStr], dir: &OsStr) -> Result<ExitStatus, Error> {
-    //let _span = tracing::info_span!("foo").entered();
-    tracing::info!("{} {:?} from {}", program.display(), args.iter().map(|arg| arg.display()).collect::<Vec<_>>(), dir.display());
-    let exit_status = Command::new(program)
-        .current_dir(dir)
-        .args(args)
-        .status();
-    tracing::info!("{} {:?} from {}. exit status: {:?}", program.display(), args.iter().map(|arg| arg.display()).collect::<Vec<_>>(), dir.display(), exit_status);
-    exit_status
-} */
-
-macro_rules! error_and_exit {
-    ($error:expr) => {
-        tracing::info!("Exiting due to error: {:?}", error);
-        exit(1)
-    };
-}
-
-/* fn run(cmd: &mut Command) -> anyhow::Result<()> {
-    let status = cmd.status()
-        .with_context(|| format!("failed to run {:?}", cmd))?;
-
-    if !status.success() {
-        anyhow::bail!("command exited with {}", status);
-    }
-
-    Ok(())
-} */
-
-/* fn run_tmux(cmd: &mut Command) -> Result<()> {
-    tracing::info!(command = ?cmd, "spawning command");
-
-    let status = cmd
-        // inherit stdout/stderr → visible in terminal / tmux
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .status()
-        .with_context(|| format!("failed to spawn {:?}", cmd))?;
-
-    if !status.success() {
-        anyhow::bail!("command {:?} exited with {}", cmd, status);
-    }
-
-    Ok(())
-} */
 
 fn start_demo(dir: &OsStr, session_name: &OsStr, commands: &[&OsStr]) -> Result<()> {
     tracing::info!("Starting demo. Running {:#?} from {}", commands, dir.display());
@@ -370,88 +184,3 @@ fn main() -> ExitCode {
 
     ExitCode::SUCCESS
 }
-
-// TODO: Log everything.
-/* fn main() {
-    let cli = Cli::parse();
-    let dir = cli.dir.as_os_str();
-
-    let session_name = cli.session_name;
-    let commands = cli.commands;
-
-    /* let pkill_result = Command::new("pkill")
-        .current_dir(dir)
-        .arg("ax")
-        .status(); */
-    /* let pkill_result = match run_status(OsStr::new("pkill"), &[OsStr::new("ax")], dir) {
-        Ok(exit_status) => exit_status,
-        Err(error) => exit(1),
-    }; */
-
-
-
-/*     let _ = Command::new("rm")
-        .current_dir(dir)
-        .arg("-rf")
-        .arg("ax-data")
-        .status();
-
- */
-
-    let ax_handle = Command::new("ax")
-        .current_dir(dir)
-        .arg("run")
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn();
-
-    thread::sleep(time::Duration::from_secs(1));
-
-    /* let _ = Command::new("bash")
-        .current_dir(dir)
-        .arg(script)
-        .arg(session_name)
-        .spawn(); */
-
-    /* let commands = [
-        "npm run start-transport -- WarehouseFactory; exec bash",
-        "npm run start-door -- WarehouseFactory; exec bash",
-        "npm run start-forklift -- WarehouseFactory; exec bash",
-        "npm run start-factory-robot -- WarehouseFactory; exec bash"
-
-    ]; */
-
-    let _ = split_and_run(&session_name, commands.iter().map(|cmd| cmd.as_str()).collect(), Some(dir));
-
-} */
-
-
-
-
-
-/* fn main() {
-    let cli = Cli::parse();
-    let demo_script = cli.demo_script;
-    let session_name = cli.session_name;
-    let script = demo_script.file_name().unwrap();
-    let dir = demo_script.parent().unwrap().as_os_str();
-
-    let commands = vec![
-        "npm run start-transport -- WarehouseFactory; exec bash",
-        "npm run start-door -- WarehouseFactory; exec bash",
-        "npm run start-forklift -- WarehouseFactory; exec bash",
-        "npm run start-factory-robot -- WarehouseFactory; exec bash",
-    ].into_iter()
-    .map(|c| )
-
-    let tmux = Tmux::with_commands();
-    //    .socket_name(session_name)
-
-
-    let _ = Command::new("bash")
-        .current_dir(dir)
-        .arg(script)
-        .arg(session_name)
-        .spawn();
-
-} */
