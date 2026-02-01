f0c72205e3ff402b0ab7e2af209cbf2547623508
lucas874 <lsc500@outlook.dk>
2026-02-01 14:30:36 +0100

    Update demos/warehouse-factory/kick_the_tires.sh: take log files as positional args.

--- demos/warehouse-factory/kick_the_tires.sh
+++ demos/warehouse-factory/kick_the_tires.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
+
 # $1 is tmux session name
+# $2 is log file for transport machine
+# $3 is log file for door machine
+# $4 is log file for forklift machine
+# $5 is log file for assembly robot machine
 
 version="KickTheTires"
-START_TRANSPORT="npm run start-transport -- $version transport_log.txt; exec bash"
-START_DOOR="npm run start-door -- $version door_log.txt; exec bash"
-START_FORKLIFT="npm run start-forklift -- $version forklift_log.txt; exec bash"
-START_ROBOT="npm run start-factory-robot -- $version robot_log.txt; exec bash"
+transport_log=$2
+door_log=$3
+forklift_log=$4
+robot_log=$5
+START_TRANSPORT="npm run start-transport -- $version ${transport_log}; exec bash"
+START_DOOR="npm run start-door -- $version ${door_log}; exec bash"
+START_FORKLIFT="npm run start-forklift -- $version ${forklift_log}; exec bash"
+START_ROBOT="npm run start-factory-robot -- $version ${robot_log}; exec bash"
 
 npm run build
 
